1dcc3d0665a898fda23c815d2a7b2570955a5bc7
Dina Berry <41597107+diberry@users.noreply.github.com>
2022-05-12 12:36:51 -0700

    Update index.js

--- javascript/ProcessImageUpload/index.js
+++ javascript/ProcessImageUpload/index.js
@@ -7,53 +7,60 @@ const sleep = require('util').promisify(setTimeout);
 const STATUS_SUCCEEDED = "succeeded";
 const STATUS_FAILED = "failed"
 
-async function AnalyzeImage(computerVisionClient, url){
+async function readFileUrl(context, computerVisionClient, url) {
 
-  // To recognize text in a local image, replace client.read() with readTextInStream() as shown:
-  let result = await computerVisionClient.read(url);
+    try {
 
-  // Operation ID is last path segment of operationLocation (a URL)
-  let operation = result.operationLocation.split('/').slice(-1)[0];
+        context.log(`uri = ${url}`);
 
-  // Wait for read recognition to complete
-  // result.status is initially undefined, since it's the result of read
-  while (result.status !== STATUS_SUCCEEDED) { 
-      await sleep(1000); 
-      result = await computerVisionClient.getReadResult(operation); 
-  }
+        // To recognize text in a local image, replace client.read() with readTextInStream() as shown:
+        let result = await computerVisionClient.read(url);
 
-  JSON.stringify(result.analyzeResult.readResults);
+        // Operation ID is last path segment of operationLocation (a URL)
+        let operation = result.operationLocation.split('/').slice(-1)[0];
 
-  let contents="";
+        // Wait for read recognition to complete
+        // result.status is initially undefined, since it's the result of read
+        while (result.status !== STATUS_SUCCEEDED) {
+            await sleep(1000);
+            result = await computerVisionClient.getReadResult(operation);
+        }
 
-  result.analyzeResult.readResults.map((page) => {
-      page.lines.map(line => {
-            contents += line.text + "\n\r"
-      });
-  });
-  return contents; 
+        let contents = "";
+
+        result.analyzeResult.readResults.map((page) => {
+            page.lines.map(line => {
+                contents += line.text + "\n\r"
+            });
+        });
+        return contents;
+
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = async function (context, myBlob) {
 
-    try{
-    context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
+    try {
+        context.log("JavaScript blob trigger function processed blob \n Blob:", context.bindingData.blobTrigger, "\n Blob Size:", myBlob.length, "Bytes");
 
-    const computerVision_ResourceKey = process.env.ComputerVisionKey;
-    const computerVision_Endpoint = process.env.ComputerVisionEndPoint;
+        const computerVision_ResourceKey = process.env.ComputerVisionKey;
+        const computerVision_Endpoint = process.env.ComputerVisionEndPoint;
 
-    const computerVisionClient = new ComputerVisionClient(
-        new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': computerVision_ResourceKey } }), computerVision_Endpoint);
+        const computerVisionClient = new ComputerVisionClient(
+            new ApiKeyCredentials({ inHeader: { 'Ocp-Apim-Subscription-Key': computerVision_ResourceKey } }), computerVision_Endpoint);
 
-    const textContext = await AnalyzeImage(computerVisionClient, context.bindingData.uri);
+        // URL must be full path
+        const textContext = await readFileUrl(context, computerVisionClient, context.bindingData.uri);
 
-    context.bindings.tableBinding = [];
-    context.bindings.tableBinding.push({
-        PartitionKey: "Images",
-        RowKey: uuidv4().toString(),
-        Text: textContext
-    });
-    }catch(err){
+        context.bindings.tableBinding = [];
+        context.bindings.tableBinding.push({
+            PartitionKey: "Images",
+            RowKey: uuidv4().toString(),
+            Text: textContext
+        });
+    } catch (err) {
         context.log(err);
         return;
     }
